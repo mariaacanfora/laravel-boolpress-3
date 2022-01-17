3ebc86aeb31833d1d6e43cdc84f5263df36f9d83
mariaacanfora <ma.maria.acanfora@gmail.com>
2022-01-17 17:07:45 +0100

    Added App.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container text-center">
+      <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'App', 
+    data(){
+        return {
+            msg: 'Benvenuto in WordPress'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,4 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function (){
         Route::get('/', 'HomeController@index')->name('home');
-    });
-
-/* 
-Route::get('/home', 'HomeController@index')->name('home'); */
+});
